833846803ff1286b1750a206ae172795248f43a9
Your Name <you@example.com>
2025-02-16 23:07:45 +0600

    done

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ document.getElementById("menuBarIcon").addEventListener("click", function () {
     menuBar.style.marginRight = "-300px";
 
     const menu = document.getElementById("menu");
-    menu.style.marginTop = "-10px";
-    menu.style.transition = "0.8s";
+    menu.style.marginLeft = "-180px";
+    menu.style.transition = "1s";
+    menu.style.position = "relative"
 
-    const deleteMenu = document.getElementById("deleteMenu").style.display = "block";
+    const deleteMenu = document.getElementById("deleteMenu");
+    deleteMenu.style.display = "block";
+    deleteMenu.style.left = "170px"
+    deleteMenu.style.transition = "0.8s"
 
     const user = document.getElementById("user");
     user.style.display = "block";
@@ -18,16 +22,18 @@ document.getElementById("menuBarIcon").addEventListener("click", function () {
 
 document.getElementById("deleteMenu").addEventListener("click", function () {
 
-    const menu = document.getElementById("menu");
-    menu.style.marginTop = "-300px";
-    menu.style.transition = "0.8s";
-
 
     const menuBar = document.getElementById("menuBar");
     menuBar.style.display = "block";
-    menuBar.style.marginRight = "5px";
-    menuBar.style.transitionDelay = "3s"
+    menuBar.style.marginRight = "30px";
+    menuBar.style.transition = "1s";
+
+    const menu = document.getElementById("menu");
+    menu.style.marginLeft = "-400px";
+    menu.style.transition = "0.1s";
+    menu.style.position = "absolute"
 
     const user = document.getElementById("user");
     user.style.display = "none";
+    document.getElementById("deleteMenu").style.display = "none"
 })
